fc22739dd77fbd5d1c7f1305e4cefdc44844f14b
fariharafa <40835861+fariharafa@users.noreply.github.com>
2018-07-31 10:55:30 -0400

    Add files via upload

---
+++ adventure.py
@@ -0,0 +1,34 @@
+answer = input("Summer has started. Would rather go the beach or to the movies?")
+
+if answer == 'beach' or answer == 'go to the beach' or answer == "Beach" or answer ==  "Go to the beach" or answer ==  "Go to the Beach" or answer ==  "BEACH":
+    print("                ")
+    answerb = input ("What do you want to do? Eat or play volleyball?")
+    if answerb == "eat" or answerb == "EAT" or answerb == "Eat":
+        print ("LET'S GO EAT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+else:
+    print('the movies sounds better')
+    answerm =input ("Are you gonna go alone or with a special friend?")
+    if answerm == ("alone" or "go alone"):
+        print ("You're such a loner but whatever.")
+        answerm2 = input("You walk into the movies and see your ex with his/her significant other. What do you do? Do you walk away or confront them?")
+        if answerm2 == "walk away" or answerm2 == "walk":
+            answerm3 = input("are you sure?")
+            if answerm3 == "yes":
+                print ("you're a loser. anyways.......")
+
+
+            else:
+                print ("I'm in the mood for some drama")
+                answerm4 = input("Do you want to go up to them with an attitude or politely?")
+                if answerm4 == "attitude" or answerm4 == "with attitude":
+                    print ("So I've seen you've downgraded")
+                    answerm5 = input("What are you gonna tell your ex?")
+                    print ("You go girl!!!!!!")
+                else:
+                    print ("No you can't be polite. You should've chose attitude")
+                    answerm5 = input("What are you gonna tell your ex?")
+                    print ("You go girl!!!!!!")
+
+        else:
+            print ("Im in the mood for some drama")
+            answerm4 = input("Do you want to go up to them with an attitude or politely?")
